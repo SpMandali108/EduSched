56e5c37b28895719d7c9fdb67a5b174d5616e6ea
SpMandali108 <sdhmandali80@gmail.com>
2026-04-28 19:01:59 +0530

    Changed Main.py For Gunicorn Error

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from website import create_app
 import os
 
-main = create_app()
+app = create_app()
 
 if __name__ == '__main__': 
     port = int(os.environ.get("PORT", 5000))
-    main.run(host='0.0.0.0', port=port,debug=True)
+    app.run(host='0.0.0.0', port=port,debug=True)
